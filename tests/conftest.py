55bb2d3e0a1ae7bb6c188d761abc7b30c5eb2643
Jean Henry <jean.henry@ansys.com>
2025-02-25 18:06:26 +0100

    feat: add obfuscator implementation and unit tests

---
+++ tests/conftest.py
@@ -0,0 +1,131 @@
+# -*- coding: utf-8 -*-
+
+# Copyright (C) 2025 ANSYS, Inc. and/or its affiliates.
+# SPDX-License-Identifier: MIT
+#
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+"""Unit tests fixtures."""
+
+from pathlib import Path
+import random
+from shutil import copytree, rmtree
+from typing import Tuple
+
+import pytest
+import scade_env  # noqa: F401
+
+# must be imported after scade_env
+# isort: split
+import scade
+import scade.model.project.stdproject as std
+import scade.model.suite as suite
+
+# initialize the seed and store the state to get reproducible tests
+random.seed('obfuscator')
+seed = random.getstate()
+
+
+def pytest_configure(config):
+    """Declare the markers used in this project."""
+    config.addinivalue_line('markers', 'project: project to be loaded')
+
+
+@pytest.fixture(scope='session')
+def local_tmpdir():
+    """Create/empty the temporary directory for output files."""
+    path = Path('tests') / 'tmp'
+    try:
+        rmtree(str(path))
+    except FileNotFoundError:
+        pass
+    path.mkdir()
+    return path
+
+
+def load_session(pathname: Path) -> suite.Session:
+    """
+    Load a Scade model in a separate environment.
+
+    Note: The model can have unresolved references since the libraries
+    are not loaded.
+    """
+    session = suite.Session()
+    session.load2(str(pathname))
+    assert session.model
+    return session
+
+
+def load_project(pathname: Path) -> std.Project:
+    """
+    Load a Scade project in a separate environment.
+
+    Note: Undocumented API.
+    """
+    project_ = scade.load_project(str(pathname))
+    return project_
+
+
+def load_tmp_project_session(path: Path, target_dir: Path) -> Tuple[std.Project, suite.Session]:
+    """Load a temporary copy of the project."""
+    # duplicate the project to edit it safely
+    copytree(path.parent, target_dir)
+    path = str(target_dir / path.name)
+    # TODO: libraries
+    project_ = load_project(path)
+    session = load_session(path)
+    return project_, session
+
+
+@pytest.fixture(scope='function')
+def project_session(request) -> Tuple[std.Project, suite.Session]:
+    """
+    Load a project and the corresponding Scade model.
+
+    Specify the project to load with the marker ``project``.
+    """
+    marker = request.node.get_closest_marker('project')
+    # marker is None if the test is not designed correctly
+    assert marker
+    pathname = marker.args[0]
+    project_ = load_project(pathname)
+    session = load_session(pathname)
+    return project_, session
+
+
+@pytest.fixture(scope='module')
+def tmp_project_session(local_tmpdir, request) -> Tuple[std.Project, suite.Session]:
+    """
+    Load a temporary copy of the project.
+
+    Specify the project to load with the marker ``project``.
+    """
+    marker = request.node.get_closest_marker('project')
+    # marker is None if the test is not designed correctly
+    assert marker
+    pathname = marker.args[0]
+    # duplicate the project to edit it safely
+    target_dir = local_tmpdir / pathname.stem
+    copytree(pathname.parent, target_dir)
+    pathname = str(target_dir / pathname.name)
+    # TODO: libraries
+    project_ = load_project(pathname)
+    session = load_session(pathname)
+    return project_, session
---
+++ tests/test_obfuscator.py
@@ -0,0 +1,96 @@
+# -*- coding: utf-8 -*-
+
+# Copyright (C) 2025 ANSYS, Inc. and/or its affiliates.
+# SPDX-License-Identifier: MIT
+#
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+"""
+Test suite for obfuscator.py.
+
+Test strategy:
+
+The tests of this module make a copy of a reference project, obfuscate it and add
+compare the result files to a reference.
+"""
+
+from os.path import relpath
+from pathlib import Path
+import random
+
+import pytest
+
+from ansys.scade.power_scripts.obfuscator.obfuscator import Obfuscator
+from conftest import load_tmp_project_session, seed
+from test_utils import cmp_file, get_resources_dir
+
+
+def format_test_data(data: list) -> list:
+    """Set the test id to the name of the fields project and conf."""
+    return [pytest.param(*_, id=('-'.join(_[0:2])) if _[1] else _[0]) for _ in data]
+
+
+@pytest.mark.parametrize(
+    'base, conf, libraries, internals',
+    format_test_data(
+        [
+            ('Nominal', 'Default', [], False),
+            ('Nominal', 'Internals', [], True),
+            ('Libraries', 'None', ['First', 'Second'], False),
+            ('Libraries', 'NoFirst', ['First'], False),
+            ('Libraries', 'NoSecond', ['Second'], False),
+            ('Libraries', 'All', [], False),
+        ]
+    ),
+)
+def test_obfuscator_nominal(capsys, base, conf, libraries, internals, local_tmpdir):
+    source = get_resources_dir() / 'resources' / base / 'model' / (base + '.etp')
+    target_dir = local_tmpdir / 'test_obfuscator_nominal' / base / conf
+    project, session = load_tmp_project_session(source, target_dir)
+    # make sure the tests provide always the same results
+    # seed = reduce(lambda x, y: x + ord(y), base + conf, 0)
+    random.setstate(seed)
+    # options
+    path = Path(project.pathname)
+    trace = path.with_name('trace.txt')
+    # run the obfuscation
+    cls = Obfuscator(str(trace), internals, libraries)
+    status = cls.main(session)
+    assert status == 0
+    # get the reference directory
+    ref_dir = get_resources_dir() / 'resources' / base / 'reference' / conf
+    # ignore banner if any
+    captured = capsys.readouterr()
+    # compare all the files present in ref_dir to those in target_dir
+    for reference in (ref_dir).glob('**/*'):
+        if reference.is_dir():
+            continue
+        base = relpath(reference, ref_dir)
+        target = target_dir / base
+        try:
+            diff = cmp_file(reference, target, n=0)
+        except BaseException as e:
+            diff = [str(e)]
+        # not captured, thus the loop hereafter
+        # stdout.writelines(diff)
+        for line in diff:
+            print(line, end='')
+    captured = capsys.readouterr()
+    assert captured.out == ''
